9f7258f4859f936252484c259c71e5e098fe4321
priyanka7569 <kota.priyanka@mphasis.com>
2023-11-27 17:14:57 +0530

    Day16 Assignment modified

---
+++ ConAppAssignment  21/Class.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppAssignment_21
+{
+    public class Class
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtarct(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multi(int a, int b)
+        {
+            return a * b;
+        }
+        public int Div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
